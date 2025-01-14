1bf616f32551fc4f22a51289f23f690817dee51e
Homishin <homieshin@gmail.com>
2025-01-14 23:05:18 +0500

    Методы добавления, удалени и вывода

--- src/main/java/org/example/TaskManager.java
+++ src/main/java/org/example/TaskManager.java
@@ -1,5 +1,25 @@
 package org.example;
 
-public class TaskManager {
+import java.util.ArrayList;
 
+public class TaskManager {
+    ArrayList<Task> taskList;
+
+    public TaskManager() {
+        taskList = new ArrayList<>();
+    }
+
+    public void addTask(Task task){
+        taskList.add(task);
+    }
+
+    public void removeTaskById(int id){
+        taskList.removeIf(task -> task.getId() == id);
+    }
+
+    public void getAllTasks(){
+        for(Task task : taskList){
+            System.out.println(task);
+        }
+    }
 }
